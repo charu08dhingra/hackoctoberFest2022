a986e3c5b024199ef9a2f07173f8e799e62b1d78
mrigankabrahma <mrigankabrahma222@gmail.com>
2022-10-11 11:36:29 +0530

    greater

---
+++ greater.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class coomon {
+    public static void main(String args[])
+    {
+       Scanner sc= new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       if(a==b)
+       {
+        System.out.println("a is eqal to b");
+       } 
+       else if(a>b)
+       {
+        System.out.println("a is greater");
+       }
+       else
+       {
+        System.out.println("b is greater");
+       }
+    }
+    
+}
